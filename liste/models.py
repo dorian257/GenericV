65d7107df677cccf915b99d70ee93abc69be1e92
Dorian <dorianniyongabo@gmail.com>
2020-06-08 17:54:44 +0200

    Quelque ajustement sur les pages et creation d'un custum manager

--- liste/models.py
+++ liste/models.py
@@ -1,6 +1,24 @@
+from django.contrib.admin.templatetags.admin_list import result_list
 from django.db import models
 
+
+
 # Create your models here.
+
+"""Les deux classes suivant permettent de creer un custom manager qui filtre un prenom"""
+
+class PersonneQuery(models.QuerySet):
+    def prenom(self):
+        return self.filter(Prenom='Dorian')
+
+
+class PersonneManager(models.Manager):
+    def get_queryset(self):
+        return PersonneQuery(self.model,using=self._db)
+
+
+    def prenom(self):
+        return self.get_queryset().prenom()
 
 class Personne (models.Model):
 
@@ -9,5 +27,11 @@ class Personne (models.Model):
     Profession = models.CharField(max_length=50)
     photo = models.ImageField()
 
+    objects = PersonneManager()
+
 def __str__(self):
         return self.Prenom
+
+
+
+
