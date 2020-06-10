038bd04d82ed24c937854418bb4740f2f6f40877
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2020-06-10 08:35:13 +0200

    DB to sqlite

--- Personnel/urls.py
+++ Personnel/urls.py
@@ -23,6 +23,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('liste/',include('liste.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]+ static (settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
